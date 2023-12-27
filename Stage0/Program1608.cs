e0d6775a8d102947802929862d64a7bc8d0d08d4
Eithan1108 <ekline1108@gmail.com>
2023-12-27 20:32:04 +0200

    Added new Class and some needed changes in the old class

--- Stage0/Program1608.cs
+++ Stage0/Program1608.cs
@@ -1,18 +1,24 @@
 ﻿using System;
-internal class Program
+namespace Stage0
 {
-    private static void Main(string[] args)
+
+    internal partial class Program
     {
-        Welcome1608();
-        Console.ReadKey();
+        private static void Main(string[] args)
+        {
+            Welcome1608();
+            Welcome3186();
+            Console.ReadKey();
 
 
-    }
+        }
 
-    private static void Welcome1608()
-    {
-        Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
-        Console.WriteLine($"{name}, welcome to my first console application");
+        static void Welcome1608()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}, welcome to my first console application");
+        }
+        static partial void Welcome3186();
     }
 }
