c5231dd231a4c828aca23a7ed03335577c908ed4
MAOZD <maoz2231@gmail.com>
2023-12-27 19:41:24 +0200

    Initial code+ welcome functions

---
+++ Stage0/Program1608.cs
@@ -0,0 +1,18 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome1608();
+        Console.ReadKey();
+
+
+    }
+
+    private static void Welcome1608()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"{name}, welcome to my first console application");
+    }
+}
